ea3558ec3b58f3aa2d0e83fca6a724fe65500638
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-10-30 20:35:40 +0100

    First Draft

--- app/src/main/java/com/example/ntp_clock/MainActivity.java
+++ app/src/main/java/com/example/ntp_clock/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -18,11 +19,20 @@ import com.example.ntp_clock.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private TextView textViewLabelTime; //Deklarera TextView för tid.
+    private TextView textViewClock; //Deklarera TextView för klocka.
+    private Handler handler = new Handler(); // Initiera handler för min tickande klocka.
+    private SimpleDateFormat clock = new SimpleDateFormat("HH:MM:SS", Locale.getDefault()); //Initiera min klocka för att formatera tidvärdena som strängar i formatet "HH:MM:SS"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +47,29 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        //Initiera TextView komponenter
+        textViewLabelTime = findViewById(R.id.textViewLableTime);
+        textViewClock = findViewById(R.id.textViewClock);
+        //Uppdatera tiden på displayen genom att schemalägga en upprepad uppgift med hjälp av en handler
+        //Anropa metoden updateTimeOnNetwork() för att uppdatera den visade tiden varje sekund.
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                updateTimeOnNetWork(); //Kallar på metoden
+            }
+        }, 1000);
+        super.onResume();
+    }
+
+    //Metod för att få systemtiden.
+    private String getSystemTime(){
+        return clock.format(new Date()); // Formatera tiden som "HH:MM:SS"
+    }
+
+
+
+        /*binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -45,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
