52e5265fa66b721c5ce28a4a0969d7e000416797
Josefina <112643341+Data-Joos@users.noreply.github.com>
2023-10-31 12:57:20 +0100

    Whole code

--- app/src/main/java/com/example/ntp_clock/MainActivity.java
+++ app/src/main/java/com/example/ntp_clock/MainActivity.java
@@ -1,38 +1,39 @@
 package com.example.ntp_clock;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.Network;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Handler;
-import android.view.View;
-
-import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.ntp_clock.databinding.ActivityMainBinding;
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-
+import java.io.IOException;
+import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    private TextView textViewLabelTime; //Deklarera TextView för tid.
-    private TextView textViewClock; //Deklarera TextView för klocka.
-    private Handler handler = new Handler(); // Initiera handler för min tickande klocka.
-    private SimpleDateFormat clock = new SimpleDateFormat("HH:MM:SS", Locale.getDefault()); //Initiera min klocka för att formatera tidvärdena som strängar i formatet "HH:MM:SS"
+    private TextView textViewLabelTime;
+    private TextView textViewClock;
+    private Handler handler = new Handler();
+    private SimpleDateFormat clock = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,64 +48,72 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        //Initiera TextView komponenter
-        textViewLabelTime = findViewById(R.id.textViewLableTime);
+        textViewLabelTime = findViewById(R.id.textViewLabelTime);
         textViewClock = findViewById(R.id.textViewClock);
-        //Uppdatera tiden på displayen genom att schemalägga en upprepad uppgift med hjälp av en handler
-        //Anropa metoden updateTimeOnNetwork() för att uppdatera den visade tiden varje sekund.
+        clock.setTimeZone(TimeZone.getTimeZone("Europe/Stockholm"));
+
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 handler.postDelayed(this, 1000);
-                updateTimeOnNetWork(); //Kallar på metoden
+                updateTimeOnNetWork();
             }
         }, 1000);
         super.onResume();
     }
 
-    //Metod för att få systemtiden.
     private String getSystemTime(){
-        return clock.format(new Date()); // Formatera tiden som "HH:MM:SS"
+        return clock.format(new Date());
     }
 
+    private String getNTPTime(){
+        NTPUDPClient ntpClient = new NTPUDPClient();
+        ntpClient.setDefaultTimeout(3000);
+        try {
+            InetAddress inetAddress = InetAddress.getByName("3.se.pool.ntp.org");
+            TimeInfo timeInfoServer = ntpClient.getTime(inetAddress);
+            long ntpTime = timeInfoServer.getReturnTime();
+            return clock.format(new Date(ntpTime));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error: " + e.getMessage();
+        } finally {
+            ntpClient.close();
+        }
+    }
 
-        /*binding.fab.setOnClickListener(new View.OnClickListener() {
+    private boolean isMyNetworkAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    private void updateTimeOnNetWork(){
+        if (isMyNetworkAvailable()){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String networkTimeText = getNTPTime();
+                    updateUserInterface(networkTimeText, "Network time", Color.GREEN);
+                }
+            }).start();
+        }else {
+            String systemTimeText = getSystemTime();
+            updateUserInterface(systemTimeText, "System time", Color.RED);
+        }
+    }
+    private void updateUserInterface(String time, String label, int labelColor) {
+        runOnUiThread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void run() {
+                textViewClock.setText(time);
+                textViewLabelTime.setText(label);
+                textViewLabelTime.setTextColor(labelColor);
             }
         });
-    }*/
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-}
+    }}
